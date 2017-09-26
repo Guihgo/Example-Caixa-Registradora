52964adb3791d2acc0389c6b7f97f906e39e23ba
Guilherme Henrique <guihgo100milha@gmail.com>
2017-09-26 01:11:39 +0000

    ok

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 /* structs */
 typedef struct {
     int codigo;
+    double quantidade;
+} Carrinho;
+
+typedef struct {
+    int codigo;
     char descricao[150];
     double quantidade, preco;
 } ItemEstoque;
@@ -14,11 +19,15 @@ typedef struct {
     int nLinhas;
 } EstoqueData;
 
+
 /* Declarações de funções */
 EstoqueData * leEstoque();
 double existeEstoque(int codigo);
-//float Soma(int codigo, int quantidade);
+//float soma(int codigo, int quantidade);
 //float troco(float total, float vrecebe);
+
+/* Declaracoes de metodos */
+void criaNovaCompra();
 
 /* Variaveis globais */
 EstoqueData estoqueData;
@@ -35,71 +44,31 @@ int main() {
     // printf("\n&estoqueData - main(): %d ", &estoqueData);
     // printf("\n*estoqueData - main(): %d ", *estoqueData);
     
-    printf("\n\n----Testandooo-----") ;
-    for(int p=0; p<estoqueData.nLinhas; p++) {
-        printf("\n->Linha[%d]", p);
-        printf("\n   ->Codigo: %d", estoqueData.itensEstoque[p].codigo);
-        printf("\n   ->Descricao: %s", estoqueData.itensEstoque[p].descricao);
-    }
+    // printf("\n\n----Testandooo-----");
+    // for(int p=0; p<estoqueData.nLinhas; p++) {
+    //     printf("\n->Linha[%d]", p);
+    //     printf("\n   ->Codigo: %d", estoqueData.itensEstoque[p].codigo);
+    //     printf("\n   ->Descricao: %s", estoqueData.itensEstoque[p].descricao);
+    // }
  
-    /*
-    
-    int b,a;
-    
-    float total=0;
-    
-    while( b!=0 ){
-    
-        printf("1 - Novo Produto\n0 - Finalizar");
-        scanf("%d", a);
-    
+    int a = 0;
+    while(a!=1) {
+        printf("\nEscolha uma opcao:");
+        printf("0 - Novo Compra\n1 - Finalizar");
+        scanf("%d", &a);
+        
         switch(a){
-         
-        case 0:
-        float vrecebe,troco;
-            printf("Valor total = %f", total);
-            printf("Valor Recebido:");
-                scanf("%f", &vrecebe).
-                troco = troco(total,vrecebe);
-                printf("\nValor do Troco: %f",troco);
+            case 0:
+                criaNovaCompra();
             break;
             
-        case 1:
-            int op;
-            printf("1 - Digitar ou 2 - Escanear");
-                scanf("%d", &op);
-               
-            if(op == 1){
-                int codigo, quantidade;
-                
-                printf("Codigo do Produto:");
-                scanf("%d", &codigo);
-                printf("Quantidade");
-                scanf("%d", &quantidade)
-                    
-                    total = total + Soma(codigo,quantidade);
-                
-            }
-         break;
-      
-        
+            case 1: //faz nada
+            break;
             
+            default:
+                printf("\nEssa opcao nao existe!\nTente novamente");
+            break;
         }
-     
-
-    }
-    */
-    
-    
-    // FILE *fstream = fopen("teste.csv","w");
-    // if(fstream == NULL) {
-    //     printf("\n Erro ao abrir arquivo teste.csv");
-    //     return -1;
-    // }
-    
-    // fputs("Testeeeee...\n", fstream);
-    
-    // fclose(fstream);
     
     return 0;
 }
@@ -184,94 +153,16 @@ EstoqueData * leEstoque() {
     }
     fclose(fstream);
     
+
     EstoqueData estoqueData = {itensEstoque, nLinhas};
     EstoqueData * pEstoqueData = &estoqueData;
     // printf("\n&estoqueData - leEstoque(): %d ", &estoqueData);
     return pEstoqueData;
 }
 
-/*
-EstoqueData leEstoque() {
-    char buffer[1024];
-    char *coluna, *linha;
-    int i=0,j=0;
-    
-    FILE *fstream = fopen("estoque.csv","r");
-    if(fstream == NULL) {
-        printf("\n Erro ao abrir arquivo estoque.csv");
-        EstoqueData estoqueData;
-        return estoqueData;
-    }
-    
-    //contando quantas linhas tem
-    char ch;
-    int nLinhas=0;
-    
-    while(!feof(fstream)) {
-      ch = fgetc(fstream);
-      if(ch == '\n')
-      {
-        nLinhas++;
-      }
-    }
-    printf("nLinhas: %d", nLinhas);
-    
-    ItemEstoque itensEstoque[nLinhas+2]; //cria vetor que ira conter todos itens do estoque
-    
-    //fseek(fstream, 0, SEEK_SET); //rebobina fstream
-    rewind(fstream); //rebobina fstream
-    
-    //enquanto tiver uma linha pra ser lida
-    while((linha = fgets(buffer,sizeof(buffer),fstream))!=NULL) {
-        //printf("\nLinha[%d]: %s", i, linha) ;
-        
-        j=0; //contador de coluna
-        
-        coluna = strtok(linha,";"); //procura primeiro delimitador na linha e corta a linha até ele
-        while(coluna!=NULL) //enquanto tiver delimitadores na linha
-        {
-            //printf("\nColuna[%d] : %s", j, coluna);
-            
-            switch(j) {
-                case 0: //coluna codigo
-                    itensEstoque[i].codigo = atoi(coluna);
-                break;
-                case 1: //coluna descricao
-                    strcpy(itensEstoque[i].descricao, coluna);
-                    printf("%s",itensEstoque[i].descricao);
-                break;
-                case 2: //coluna quantidade
-                    itensEstoque[i].quantidade = atof(coluna);
-                break;
-                case 3: //coluna preço
-                    itensEstoque[i].preco = atof(coluna);
-                break;
-                default:
-                break;
-            }
-            
-            j++;
-            coluna = strtok(NULL,";"); //procura proximo delimitador e corta
-        }
-        i++;
-    }
-    
-    EstoqueData estoqueData = {itensEstoque, nLinhas};
-    fclose(fstream);
-    
-    printf("\n\n----Testando 0-----") ;
-    for(int p=0; p<estoqueData.nLinhas; p++) {
-        printf("\n->Linha[%d]", p);
-        printf("\n   ->Codigo: %d", estoqueData.itensEstoque[p].codigo);
-        printf("\n   ->Descricao: %s", estoqueData.itensEstoque[p].descricao);
-    }
-    
-    return estoqueData;
-}
-*/
 
 /*
-float Soma(int codigo, int quantidade){
+float soma(int codigo, int quantidade){
     float soma = 0;
     
     for(int p=0; p<estoqueData.nLinhas; p++){
@@ -296,3 +187,40 @@ float troco(float total,float vrecebe){
 }
 
 */
+
+
+void criaNovaCompra(){
+    int a = 0;
+    
+    while(a!=1){
+        printf("\n0 - Novo Produto\n1 - Finalizar");
+        scanf("%d", &a);
+        switch(a){
+            
+            case 0:
+                int b;
+                printf("\n0 - Digitar codigo\n1 - Escanear QRcode");
+                scanf("%d", &b);
+                
+                if(b==0){
+                    int codigo, quantidade;
+                    
+                    printf("Codigo do Produto:");
+                    scanf("%d", &codigo);
+                    printf("Quantidade");
+                    scanf("%d", &quantidade)
+                    
+                    total = total + soma(codigo,quantidade);
+                }
+                
+                if(b==1){
+                    //deixa pra depois que td estiver pronto...
+                }
+            break;
+            
+            case 1:
+                //checkout();//total, troco, saida estoque;
+            break;
+        }
+    }
+}
