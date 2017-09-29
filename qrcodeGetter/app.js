857102645aae30401a26505db30fb03ad8dc2af5
Guilherme Henrique <guihgo100milha@gmail.com>
2017-09-29 00:13:42 +0000

    qrcodeGetter ok

--- qrcodeGetter/app.js
+++ qrcodeGetter/app.js
@@ -9,10 +9,11 @@ var app = express();
 var server = require('http').createServer(app); //cria servidor http utilizando express como intermediador
 var bodyParser = require('body-parser');
 var fs = require('fs');
-
-
 server.listen(process.env.PORT || 80); //inicia servidor http na porta 80
-console.log('Server http  rodando...');
+var SocketIo = require('socket.io')(server);
+
+
+console.log('Server http e socket  rodando...');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());// parse application/json
@@ -31,4 +32,66 @@ app.post('/setCodigo', function(req, res) {
     
         console.log("O codigo "+req.body.codigo+" foi salvo");
     });
+    res.send({msg:'ok'});
+});
+
+var oldCompraFeita = "";
+fs.watch('./compraFeita.csv', (eventType, filename) => {
+  if (filename) {
+    console.log(filename+ " - " + eventType);
+    fs.readFile('./'+filename, function (err, data) {
+      if (err) {
+        return console.error(err);
+      }
+      if(oldCompraFeita != data.toString()) {
+        //console.log("Asynchronous read: \n", data.toString());
+        var itensComprados = [];
+        data.toString().split("\n").forEach(function(linha, iLinha){
+            var item = {
+                descricao: "",
+                quantidade: 0,
+                valor: 0
+            };
+            itensComprados.push(item);
+            linha.split(";").forEach(function(coluna, iColuna){
+                switch(iColuna) {
+                case 0:
+                    itensComprados[iLinha].descricao = coluna;
+                    break;
+                case 1:
+                    itensComprados[iLinha].quantidade = coluna;
+                    break;
+                case 2:
+                    itensComprados[iLinha].valor = coluna;
+                    break;
+                default:
+                    break;
+                }
+            });
+        });
+        var isNovaCompra = (itensComprados[0].descricao=="") ? true:false;
+        SocketIo.sockets.emit('compraFeita', {"itensComprados": itensComprados, "isNovaCompra":isNovaCompra});
+      }
+      oldCompraFeita = data.toString();
+    });
+  }
+});
+
+SocketIo.sockets.on('connection', function(socket){ //quando houver uma nova conexão
+    console.log('Um client conectou');
+    
+	//socket.emit('postMsg', {msg: theMsg}); //envia a msg para o socket que acabou de conectar
+    //SocketIo.sockets.emit('nUsersOnline', {nUsersOnline: connections.length});
+    
+	//OnDisconect - quando um socket desconectar
+	socket.on('disconnect', function(data) { //quando desconectar
+		console.log('Um client desconectou... ', data);
+	});
+
+	 //Quando receber uma mensagem
+// 	socket.on('msgEnviada', function(data) {
+// 		console.log(data);
+// 		SocketIo.sockets.emit('postMsg', {msg: data}); //envia para todos sockets abertos a mensagem que acabou de ser recebida
+// 	});
+
 });
